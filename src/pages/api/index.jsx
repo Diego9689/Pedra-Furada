e58acb9f7a9e039e81ba8c5241bf0641497b8037
Diego Vinicius <coelhodiego059@gmail.com>
2025-10-10 15:45:40 +0000

    altercões do index

--- src/pages/api/index.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() { 
-return ( 
-<main> 
-<h1>Bem-vindo à Pousada Pedra Furada</h1> 
-</main> 
-); 
-} 
---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Bem-vindo à Pousada Pedra Furada</h1>
+      <p>Seu refúgio perfeito na natureza.</p>
+    </main>
+  );
+}
