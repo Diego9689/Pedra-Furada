82ee357cdabefd5e2f2f6d08ddb0313731d034d7
Diego Vinicius <coelhodiego059@gmail.com>
2025-10-10 16:00:43 +0000

    rodar projeto final

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,12 +1,12 @@
- 
 import { render, screen } from "@testing-library/react"; 
 import Home from "../pages/index"; 
+
 describe("Página inicial", () => { 
 it("deve renderizar o título principal", () => { 
-const { container } = render(<Home />); 
-console.log(container.innerHTML); // mostra o HTML renderizado 
+    const { container } = render(<Home />); 
+    console.log(container.innerHTML); //  mostra o HTML renderizado 
 expect( 
-screen.getByText("Bem-vindo à Pousada Pedra Furada") 
+    screen.getByText("Bem-vindo à Pousada Pedra Furada") 
 ).toBeInTheDocument(); 
 }); 
 }); 
